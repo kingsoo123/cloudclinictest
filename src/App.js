7399369003b74af45dee0d2bfe196f498260e480
Gabriel John <gabrieljohn@Gabriels-MacBook-Pro.local>
2023-05-06 09:58:34 +0100

    started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchUsers } from './actions/userActions';
 
 function App() {
+  const dispatch = useDispatch();
+  const { users, loading, error } = useSelector(state => state.users);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [genderFilter, setGenderFilter] = useState('all');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage] = useState(10);
+
+  const handleChangeSearchTerm = e => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleChangeGenderFilter = e => {
+    setGenderFilter(e.target.value);
+    setCurrentPage(1);
+  };
+
+  const handleClearFilters = () => {
+    setSearchTerm('');
+    setGenderFilter('all');
+    setCurrentPage(1);
+  };
+
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+
+  const filteredUsers = users.filter(user => {
+    const name = user.name.toLowerCase();
+    const username = user.username.toLowerCase();
+    const searchTermLower = searchTerm.toLowerCase();
+    const gender = user.gender.toLowerCase();
+    return (
+      (name.includes(searchTermLower) || username.includes(searchTermLower)) &&
+      (genderFilter === 'all' || genderFilter === gender)
+    );
+  });
+
+  const currentUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser);
+
+  const renderTableHeader = () => {
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Website</th>
+        </tr>
+      </thead>
+    );
+  };
+
+  const renderTableBody = () => {
+    return (
+      <tbody>
+        {currentUsers.map(user => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>{user.email}</td>
+            <td>{user.phone}</td>
+            <td>{user.website}</td>
+          </tr>
+        ))}
+      </tbody>
+    );
+  };
+
+  const renderPagination = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(filteredUsers.length / usersPerPage); i++) {
+      pageNumbers.push(i);
+    }
+    return (
+      <ul className="pagination">
+        {pageNumbers.map(number => (
+          <li key={number} className={`page-item ${currentPage === number ? 'active' : ''}`}>
+            <button className="page-link" onClick={() => setCurrentPage(number)}>
+              {number}
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
